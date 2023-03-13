4b47e16d1362811e227a5773f9baf1404e8c8d18
Dima Khomko <khomko-dima@mail.ru>
2023-03-13 22:20:00 +0300

    cycles
    loop, while, for

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,26 @@ fn main() {
     }
 
     let num = if boolean { 1 } else { 0 };
-    println!("num {}", num)
+    println!("num {}", num);
+
+    let mut counter = 0;
+    loop {
+        if counter == 3 {
+            break;
+        }
+
+        println!("loop: {}", counter);
+        counter += 1;
+    }
+    let mut counter2 = 0;
+    while counter2 <= 3 {
+        println!("while: {}", counter2);
+        counter2 += 1;
+    }
+
+    for i in 0..5 {
+        println!("for: {}", i);
+    }
 }
 //integer, 1 5 7 9
 //i8 i16 i32 i64 i128 isize
