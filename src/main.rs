0641599a53f821fd5a0844a1c3cf0eef137866bd
Dima Khomko <khomko-dima@mail.ru>
2023-03-13 22:07:13 +0300

    variables

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
-    let age = 28;
-    println!("{age}");
+    let mut age: i8 = 28;
+    let name = String::from("Dima");
+    let symbol: char = 'F';
+    let boolean: bool = true;
+
+    println!("My name {name}, my age {age}");
+    println!("{}, {}", symbol, boolean)
 }
 //integer, 1 5 7 9
+//i8 i16 i32 i64 i128 isize
