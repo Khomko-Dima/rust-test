11f1d5cd65098ebf64fc602a7c2cdd72af3b77c3
Dima Khomko <khomko-dima@mail.ru>
2023-03-13 22:12:20 +0300

    operators
    Условные операторы

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,16 @@ fn main() {
     let boolean: bool = true;
 
     println!("My name {name}, my age {age}");
-    println!("{}, {}", symbol, boolean)
+    println!("{}, {}", symbol, boolean);
+
+    if age >= 18 {
+        println!("Have 18")
+    } else {
+        println!("Don't have 18")
+    }
+
+    let num = if boolean { 1 } else { 0 };
+    println!("num {}", num)
 }
 //integer, 1 5 7 9
 //i8 i16 i32 i64 i128 isize
